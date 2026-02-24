ae461c0fcfbf90272a02179d4b9b7f9deb2be721
Gustavo Beltrami <G.Beltrami@MacBook-Air-7.local>
2026-02-24 19:51:16 -0300

    fix: Redirect carreiras.patronos.org to centro.patronos.org
    Consolidate to single domain to avoid CORS issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,8 @@
+// Redirect carreiras.patronos.org to centro.patronos.org (single domain)
+if (window.location.hostname === 'carreiras.patronos.org') {
+  window.location.replace(window.location.href.replace('carreiras.patronos.org', 'centro.patronos.org'));
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
